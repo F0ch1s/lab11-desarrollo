dd49710350e92f9ce15ac2490909b0e9e217c9d1
PieroCahuana <piero.cahuana@ucsm.edu.pe>
2024-11-20 22:08:51 -0500

    Se desarrolla y prueba el uso de los tokens en la exp2

--- Experiencia02/index.js
+++ Experiencia02/index.js
@@ -8,11 +8,44 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.post("/api/posts", (req, res) => {
-  res.json({
-    mensaje: "Post Creado..",
+app.post("/api/posts", verifiToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        mensaje: "Post Creado",
+        authData,
+      });
+    }
   });
 });
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "aruiz",
+    email: "aruiz@gmail.com",
+  };
+
+  jwt.sign({ user }, "secretkey", { expiresIn: "30s" }, (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+function verifiToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 app.listen(5000, () =>
   console.log("Servidor está ejecutándose en el puerto 5000")
